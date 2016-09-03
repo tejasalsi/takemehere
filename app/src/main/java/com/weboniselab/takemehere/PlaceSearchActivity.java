95613e0587043ce31d01293829d174a59c85095f
Tanmay <karan13595@gmail.com>
2016-09-03 23:30:56 +0530

    Creating project github repo

---
+++ app/src/main/java/com/weboniselab/takemehere/PlaceSearchActivity.java
@@ -0,0 +1,13 @@
+package com.weboniselab.takemehere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaceSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_search);
+    }
+}
